ff8578c7d73ec2d78ccc98dc3dacb802bc01d6e2
pguntermann <pguntermann@me.com>
2026-07-08 21:01:07 +0200

    Initial Commit

---
+++ Chess Recorder/Chess_RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chess_RecorderApp.swift
+//  Chess Recorder
+//
+//  Created by Philipp on 08.07.26.
+//
+
+import SwiftUI
+
+@main
+struct Chess_RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chess Recorder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chess Recorder
+//
+//  Created by Philipp on 08.07.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
